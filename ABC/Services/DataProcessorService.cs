88c7fa082ffb0494a7af76a3524ce67c27a61b99
MD YASIR ARAFAT <ping@arafat.pro>
2021-11-15 16:18:43 +0600

    Update before Test Porject Add

---
+++ ABC/Services/DataProcessorService.cs
@@ -0,0 +1,19 @@
+﻿using ABC.Brokers;
+
+namespace ABC.Services
+{
+    public class DataProcessorService : IDataProcessorService
+    {
+        private readonly IStorageBroker storageBroker;
+        
+        public DataProcessorService(IStorageBroker storageBroker)
+        {
+            this.storageBroker = storageBroker;
+        }
+
+        public List<string> ProcessData()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
